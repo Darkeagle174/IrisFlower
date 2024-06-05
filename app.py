b7d74f3c99316a7d632e746672727f30bd7900e7
Dhruv Patel <143174944+Darkeagle174@users.noreply.github.com>
2024-06-05 18:41:07 +0530

    modified:   app.py 	new file:   iris-setosa.png 	new file:   iris-versicolor.png 	new file:   iris-virginica.png 	modified:   templates/index.html

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import numpy as np
 from sklearn import datasets
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
+import base64
 
 app = Flask(__name__)
 
@@ -15,6 +16,21 @@ knn = KNeighborsClassifier(n_neighbors=3)
 knn.fit(X_train, y_train)
 
 feature_ranges = [(X[:, i].min(), X[:, i].max()) for i in range(X.shape[1])]
+
+def image_to_base64(image_path):
+    with open(image_path, "rb") as img_file:
+        return base64.b64encode(img_file.read()).decode('utf-8')
+
+setosa_base64 = image_to_base64('iris-setosa.png')
+versicolor_base64 = image_to_base64('iris-versicolor.png')
+virginica_base64 = image_to_base64('iris-virginica.png')
+
+
+images = {
+    'setosa': 'data:image/jpeg;base64,' + setosa_base64,
+    'versicolor': 'data:image/jpeg;base64,' + versicolor_base64,
+    'virginica': 'data:image/jpeg;base64,' + virginica_base64
+}
 
 @app.route('/')
 def home():
@@ -31,7 +47,10 @@ def predict():
     ]).reshape(1, -1)
     prediction = knn.predict(user_input)
     pred_class = iris.target_names[prediction[0]]
-    return jsonify({'prediction': pred_class})
+
+    image_url = images[pred_class]
+
+    return jsonify({'prediction': pred_class, 'image_url': image_url})
 
 if __name__ == '__main__':
     app.run(debug=True)
