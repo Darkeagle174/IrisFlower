0913f0abb82dccce2fd26d1020110b63ab9f4ba4
Dhruv Patel <143174944+Darkeagle174@users.noreply.github.com>
2024-06-05 14:33:38 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,37 @@
+from flask import Flask, request, jsonify, render_template
+import numpy as np
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+
+app = Flask(__name__)
+
+# Load the Iris dataset and train the model
+iris = datasets.load_iris()
+X = iris.data
+y = iris.target
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+knn = KNeighborsClassifier(n_neighbors=3)
+knn.fit(X_train, y_train)
+
+feature_ranges = [(X[:, i].min(), X[:, i].max()) for i in range(X.shape[1])]
+
+@app.route('/')
+def home():
+    return render_template('index.html', feature_ranges=feature_ranges)
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = request.get_json()
+    user_input = np.array([
+        data['sepal_length'],
+        data['sepal_width'],
+        data['petal_length'],
+        data['petal_width']
+    ]).reshape(1, -1)
+    prediction = knn.predict(user_input)
+    pred_class = iris.target_names[prediction[0]]
+    return jsonify({'prediction': pred_class})
+
+if __name__ == '__main__':
+    app.run(debug=True)
